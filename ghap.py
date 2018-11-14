09bffdaec75960110966eaff8a2bd501622d169f
rosenrose <42435748+rosenrose@users.noreply.github.com>
2018-11-14 22:11:43 +0900

    Update ghap.py

--- ghap.py
+++ ghap.py
@@ -6,55 +6,96 @@ from selenium import webdriver
 
 url = "https://ghaptouhou.tistory.com"
 path = "D:/Touhou/ghap"
-driver = webdriver.Chrome("D:\\Install\\chromedriver")
+logfile = "log.log"
+driver = webdriver.Chrome("D:/Install/chromedriver")
 driver.implicitly_wait(5)
+html_footer = "</body></html>"
 
-for code in range(9, 11):
+def writeLog(msg):
+	with open("%s/%s"%(path, logfile), 'a', encoding="utf-8-sig") as a:
+		a.write(msg)
+
+def replaceSpecialCh(title):
+	res = title.replace('\\', '＼')
+	res = res.replace('/', '／')
+	res = res.replace(':','：')
+	res = res.replace('*','＊')
+	res = res.replace('?','？')
+	res = res.replace('\"','＂')
+	res = res.replace('<','〈')
+	res = res.replace('>','〉')
+	res = res.replace('|','｜')
+	return res
+
+for code in range(1, 101):
+	print("%d start" %(code))
 	driver.get("%s/%d" %(url, code))
 	soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 	tdiv = soup.find('div', { 'class': 'tdiv' })
+	if tdiv is None:
+		writeLog("%d does not exist\n" %(code))
+		continue
+
 	category = tdiv.find('div', { 'class': 'ect' }).find('a').text
-
 	catRegex = re.compile('(동방 동인지|합동인지|동방 웹코믹|세로 식질 유배소)')
-	if(catRegex.match(category)):
-		title = tdiv.find('h2').find('a').text
-		date = tdiv.find_all('span')[1].text
-		html_header = "<html><head><title>%s</title></head><body><br><h1>%s</h1><br>" %(title, title)
-		html_footer = "</body></html>"
-		
-		if not os.path.isdir("%s/%s_%d" %(path, title, code)):
-			os.mkdir("%s/%s_%d" %(path, title, code))
+	if not (catRegex.match(category)):
+		writeLog("%d out of category\n" %(code))
+		continue
 
-		f = open("%s/%s_%d.html" %(path, title, code), "w", encoding="utf-8-sig")
-		f.write(html_header)
-		article = soup.find('div', { 'class': 'article' })
+	title = tdiv.find('h2').find('a').text
+	date = tdiv.find_all('span')[1].text
+	html_header = "<html><head><title>%s</title></head><body><br><h2>%s</h2><br>" %(title, title)
 
-		p = article.find_all('p')
-		for i in p:
-			if i.find('span',{'class':'imageblock'}) is not None:
-				urllib.request.urlretrieve(i.find('img')['src'], '%s/%s_%d/%s' %(path, title, code, i.find('img')['filename']))
-				pos1 = str(i).find("<span class=\"imageblock\"")
-				pos2 = str(i).find("</p>", pos1)
-				f.write(str(i)[:pos1]+"<img src=\"%s_%d/%s\"></p><br>" %(title, code, i.find('img')['filename'])+str(i)[pos2:])
-			else:
-				f.write(str(i))
+	titleWin = replaceSpecialCh(title)
+	doc = "%s/%d_%s" %(path, code, titleWin)
+	if not os.path.isdir(doc):
+		os.mkdir(doc)
+
+	f = open(doc+".html", "w", encoding="utf-8-sig")
+	f.write(html_header)
+	f.write("%s<br>%s<br>" %(category, date))
+
+	article = soup.find('div', { 'class': 'article' })
+	p = article.find_all('p')
+	for i in p:
+		if i.find('span',{'class':'imageblock'}) is not None:
+			fileName = i.find('img')['filename']
+			urllib.request.urlretrieve(i.find('img')['src'], '%s/%s' %(doc, fileName))
+			pos1 = str(i).find("<span class=\"imageblock\"")
+			pos2 = str(i).find("</p>", pos1)
+			f.write(str(i)[:pos1]+"<img src=\"%d_%s/%s\"></p><br>" %(code, titleWin, fileName)+str(i)[pos2:])
+		else:
+			f.write(str(i))
+	writeLog("%d contents done\n" %(code))
     			
-		tag = soup.find('div', { 'class': 'tagTrail' }).find_all('a')[1:]
-		f.write("<br><p>태그: </p><ul>")
-		for i in tag:
-			f.write("<li>%s</li>"%(i.text))
-		f.write("</ul><br>")
+	tag = soup.find('div', { 'class': 'tagTrail' }).find_all('a')[1:]
+	f.write("<br><p>태그: </p><ul>")
+	for i in tag:
+		f.write("<li>%s</li>"%(i.text))
+	f.write("</ul><br>")
 
-		another = soup.find('div', { 'class': 'another_category another_category_color_gray' }).find('table').find_all('a')
-		f.write("<p>'%s' 카테고리의 다른 글</p><ul>" %(category))
-		codeRegex = re.compile('[0-9]*')
-		for i in another:
-			f.write("<li><a href=\"%s_%s.html\">%s</a></li>" %(i.text, codeRegex.findall(i['href'])[1], i.text))
-		f.write("</ul><br>")
+	another = soup.find('div', { 'class': 'another_category another_category_color_gray' }).find('table').find_all('a')
+	f.write("<p>'%s' 카테고리의 다른 글</p><ul>" %(category))
+	codeRegex = re.compile('[0-9]*')
+	for i in another:
+		anotherTitle = i.text
+		anotherTitleWin = replaceSpecialCh(anotherTitle)
+		anotherCode = codeRegex.findall(i['href'])[1]
+		f.write("<li><a href=\"%s_%s.html\">%s</a></li>" %(anotherCode, anotherTitleWin, anotherTitle))
+	f.write("</ul><br>")
 
-		comment = soup.find('div', { 'class': 'comment' })
-		f.write(str(comment))
+	comment = str(soup.find('div', { 'class': 'cb_module cb_fluid' }))
+	while(comment.find("<a href=\"/toolbar") != -1):
+		pos1 = comment.find("<a href=\"/toolbar")
+		pos2 = comment.find("</span>", pos1)
+		comment = comment[:pos1]+comment[pos2:]
+		pos1 = comment.find("<span>")
+		pos2 = comment.find("</div>", pos1)
+		comment = comment[:pos1]+comment[pos2:]
+	f.write(comment)
 
-		f.write(html_footer)
-		f.close()
+	f.write(html_footer)
+	writeLog("%d all done\n" %(code))
+	f.close()
+	print("%d end" %(code))
