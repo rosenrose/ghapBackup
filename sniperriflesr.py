ddbc32da74da1276f4ef1daa62a3d37938fb800d
rosenrose <42435748+rosenrose@users.noreply.github.com>
2018-12-12 01:30:46 +0900

    Update sniperriflesr.py

--- sniperriflesr.py
+++ sniperriflesr.py
@@ -3,6 +3,8 @@ import re
 import urllib.request
 import requests
 import time
+import bs4
+import sys
 from bs4 import BeautifulSoup
 
 url = "http://blog.daum.net/_blog/BlogTypeView.do?blogid=0qN5Q&articleno"
@@ -35,21 +37,27 @@ def sniperriflesr(codeList):
 		print("%d start" %(code))
 		response = requests.get("%s=%d" %(url, code))
 		soup = BeautifulSoup(response.content, 'html.parser')
-		main = soup.find('div', { 'class': 'articlePrint' })
+		
+		main = soup.find('div',class_='articlePrint')
 		if main is None:
 			writeLog("%d does not exist\n" %(code))
 			continue
 
-		catID = main.find('span', { 'class': 'cB_Folder' }).find('a')['href']
+		catID = main.find('span',class_='cB_Folder')
+		if catID is None:
+			writeLog("%d out of category\n" %(code))
+			continue
+
+		catID = catID.find('a')['href']
 		catID = codeRegex.findall(catID.split('&')[1])[-2]
 		if not int(catID) in catIDList:
 			writeLog("%d out of category\n" %(code))
 			continue
-		category = main.find('span', { 'class': 'cB_Folder' }).find('a').text
+		category = main.find('span',class_='cB_Folder').find('a').text
 
 		articleResponse = requests.get("%s=%d" %(articleUrl, code))
 		articleSoup = BeautifulSoup(articleResponse.content, 'html.parser')
-		if not articleSoup.find('img', {'class':'txc-image'}):
+		if not articleSoup.find('img',class_='txc-image'):
 			writeLog("%d has no image\n" %(code))
 			continue
 		
@@ -57,7 +65,7 @@ def sniperriflesr(codeList):
 		for i in title.split('-'):
 			if i.find("東方 Project") == -1:
 				title = i.strip()
-		date = main.find('span',{'class':'cB_Tdate'}).text
+		date = main.find('span',class_='cB_Tdate').text
 
 		titleWin = replaceSpecialCh(title)
 		doc = "%s/%d" %(path, code)
@@ -80,15 +88,17 @@ def sniperriflesr(codeList):
 		f.write("<div class=\"category\">%s</div><br/>\n" %(category))
 		f.write("<div class=\"date\">%s</div><br/>\n" %(date))
 
-		article = articleSoup.find('div', { 'id': 'contentDiv' })
+		article = articleSoup.find('div',id='contentDiv')
 		p = article.find_all('p')
 		f.write("<div class=\"article\">\n")
+		
 		num = 1
 		for i in p:
 			if i.find('img') is not None and i.find('img').has_attr('data-filename'):
 				imgSrc = i.find('img')['src'].replace("image","original")
 				fileExt = i.find('img')['data-filename'].split('.')[-1]
 				fileName = "%03d.%s" %(num,fileExt)
+				
 				try:
 					imgBuf = urllib.request.urlopen(imgSrc)
 				except:
@@ -110,6 +120,7 @@ def sniperriflesr(codeList):
 					imgFile.write(imgBuf)
 				finally:
 					imgFile.close()
+				
 				pos1 = str(i).find("<img")
 				pos2 = str(i).find(">", pos1)
 				f.write("\t"+str(i)[:pos1].replace("<p>","<p style=\"TEXT-ALIGN: center\">")+"<img src=\"%d_%s/%s\">" %(code,titleWin,fileName)+str(i)[pos2+1:]+"\n")
@@ -120,7 +131,7 @@ def sniperriflesr(codeList):
 		
 		f.write("<div class=\"another\">\n")
 		f.write("\t<p>'%s' 카테고리의 다른 글</p>\n\t\t<ul>\n" %(category))
-		another = soup.find('div', { 'class': 'cContentCateMore' }).find_all('li')
+		another = soup.find('div',class_='cContentCateMore').find_all('li')
 		for i in another:
 			anotherTitle = i.find('a')['title']
 			for j in anotherTitle.split('-'):
@@ -134,28 +145,75 @@ def sniperriflesr(codeList):
 			else:
 				f.write("\t\t\t<li>%s</li>\n" %(anotherTitle))
 		f.write("\t\t</ul>\n</div><br/>\n")
-		
-		comment = str(soup.find('div', { 'class': 'opinionListBox' }))
-		pos1 = comment.find("<ul class=\"opinionListMenu")
-		pos2 = comment.find("<input name=\"cmtCount")
-		pos2 = comment.find("</div>", pos2)
-		comment = comment[:pos1]+"<ul>\n"+comment[pos1:pos2]+"\n</ul>\n"+comment[pos2:]
-		
-		while(comment.find("<li class=\"opinionBtn") != -1):
-			pos1 = comment.find("<li class=\"opinionBtn")
-			pos1 = comment[:pos1].rfind("<ul class=\"opinionListMenu")
-			pos2 = comment.find("id=\"inWrite", pos1)
-			pos2 = comment.find("</div>", pos2)
-			comment = comment[:pos1]+"<li class=\"cb_comment_area\">\n"+comment[pos1:pos2+6]+"\n</li>"+comment[pos2+6:]
 
-			pos1 = comment.find("<li class=\"opinionBtn")
-			pos2 = comment.find("</ul>", pos1)
-			comment = comment[:pos1]+comment[pos2:]
-		f.write(comment)
+		comment = soup.find('div',class_="opinionListBox")
+		fulTag = soup.new_tag("ul")
+		fliTag = soup.new_tag("li",**{'class':'firstCmt'})
+		fswitch = False
+		sulTag = soup.new_tag("ul")
+		sliTag = soup.new_tag("li",**{'class':'secondCmt'})
+		sswitch = False
+		while True:
+			if isinstance(comment.contents[0], bs4.element.Tag):
+				if comment.contents[0].has_attr('class'):
+					if comment.contents[0]['class'][0] == "opinionListMenu":
+						if fswitch:
+							if sswitch:
+								sulTag.append(sliTag)
+								fliTag.append(sulTag)
+								sulTag = soup.new_tag("ul")
+								sswitch = False
+							fulTag.append(fliTag)
+						else:
+							fswitch = True
+						fliTag = soup.new_tag("li",**{'class':'firstCmt'})
 
+					elif comment.contents[0]['class'][0] == "opinionListMenuRe":
+						if sswitch:
+							sulTag.append(sliTag)
+						else:
+							sswitch = True
+						sliTag = soup.new_tag("li",**{'class':'secondCmt'})
+
+				if comment.contents[0].has_attr('type') and (comment.contents[0]['type'] == "hidden"):
+					if sswitch:
+						sulTag.append(sliTag)
+						fliTag.append(sulTag)
+					fulTag.append(fliTag)
+					comment.insert(0,fulTag)
+					break;
+			if not sswitch:
+				fliTag.append(comment.contents[0])
+			else:
+				sliTag.append(comment.contents[0])
+		
+		firstCmt = comment.contents[0].find_all('li',class_="firstCmt")
+		for i in firstCmt:
+			i.find('ul',class_="opinionListMenu").name = 'div'
+			i.find('li',class_="icon").name = 'div'
+			i.find('li',class_="fl").name = 'div'
+			if i.find('li',class_="sDateTime") is not None:
+				i.find('li',class_="sDateTime").name = 'div'
+			i.find('li',class_="opinionBtn").decompose()
+
+			if i.find('li',class_="secondCmt") is not None:
+				secondCmt = i.find_all('li',class_="secondCmt")
+				for j in secondCmt:
+					j.find('ul',class_="opinionListMenuRe").name = 'div'
+					j.find('li',class_="reIcon").name = 'div'
+					j.find('li',class_="icon").name = 'div'
+					j.find('li',class_="fl").name = 'div'
+					if j.find('li',class_="sDateTime") is not None:
+						j.find('li',class_="sDateTime").name = 'div'
+					j.find('li',class_="opinionBtn").decompose()
+		f.write(str(comment))
+		
 		f.write(html_footer)
 		f.close()
 		print("%d end" %(code))
 		time.sleep(5)
 
-sniperriflesr(range(1,5714))
+if len(sys.argv) == 2:
+	sniperriflesr([int(sys.argv[1])])
+elif len(sys.argv) == 3:
+	sniperriflesr(range(int(sys.argv[1]),int(sys.argv[2])+1))
